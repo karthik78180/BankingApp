2b8545fe17774a973f63894e51cb85daddf96e2d
karthik78180 <karthik78180@gmail.com>
2019-07-16 00:32:38 -0500

    IMplemented Spring Security feature by using Spring Security.

---
+++ src/main/java/com/userfront/controller/HomeController.java
@@ -0,0 +1,44 @@
+package com.userfront.controller;
+
+import java.security.Principal;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+import com.userfront.domain.PrimaryAccount;
+import com.userfront.domain.SavingsAccount;
+import com.userfront.domain.User;
+
+@Controller
+public class HomeController {
+	
+
+	
+	@RequestMapping("/")
+	public String home() {
+		return "redirect:/index";
+	}
+	
+	@RequestMapping("/index")
+    public String index() {
+        return "index";
+    }
+	
+	@RequestMapping(value = "/signup", method = RequestMethod.GET)
+    public String signup(Model model) {
+        User user = new User();
+
+        model.addAttribute("user", user);
+
+        return "signup";
+    }
+	
+
+}
